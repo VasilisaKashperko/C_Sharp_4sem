ffad0f1a844577adfa1edf1afc2e827a4a9b96d9
Vasilisa Kashperko <90206134+VasilisaKashperko@users.noreply.github.com>
2022-02-01 20:06:42 +0300

    The first lab under development

--- Лабораторная 1/VKLab1/VKLab1/Calculator.Designer.cs
+++ Лабораторная 1/VKLab1/VKLab1/Calculator.Designer.cs
@@ -32,9 +32,10 @@
             this.button1 = new System.Windows.Forms.Button();
             this.welcomeLettering = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxForTheFirst = new System.Windows.Forms.TextBox();
             this.inputTheFirstString = new System.Windows.Forms.Label();
             this.inputTheSecondString = new System.Windows.Forms.Label();
+            this.textBoxForTheSecond = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -57,29 +58,37 @@
             resources.ApplyResources(this.panel1, "panel1");
             this.panel1.Name = "panel1";
             // 
-            // textBox1
+            // textBoxForTheFirst
             // 
-            resources.ApplyResources(this.textBox1, "textBox1");
-            this.textBox1.Name = "textBox1";
+            resources.ApplyResources(this.textBoxForTheFirst, "textBoxForTheFirst");
+            this.textBoxForTheFirst.Name = "textBoxForTheFirst";
             // 
             // inputTheFirstString
             // 
             resources.ApplyResources(this.inputTheFirstString, "inputTheFirstString");
+            this.inputTheFirstString.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(245)))), ((int)(((byte)(250)))));
             this.inputTheFirstString.Name = "inputTheFirstString";
             // 
             // inputTheSecondString
             // 
             resources.ApplyResources(this.inputTheSecondString, "inputTheSecondString");
+            this.inputTheSecondString.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(245)))), ((int)(((byte)(250)))));
             this.inputTheSecondString.Name = "inputTheSecondString";
+            // 
+            // textBoxForTheSecond
+            // 
+            resources.ApplyResources(this.textBoxForTheSecond, "textBoxForTheSecond");
+            this.textBoxForTheSecond.Name = "textBoxForTheSecond";
             // 
             // Calculator
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.Controls.Add(this.textBoxForTheSecond);
             this.Controls.Add(this.inputTheSecondString);
             this.Controls.Add(this.inputTheFirstString);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxForTheFirst);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button1);
             this.Name = "Calculator";
@@ -95,8 +104,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label welcomeLettering;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxForTheFirst;
         private System.Windows.Forms.Label inputTheFirstString;
         private System.Windows.Forms.Label inputTheSecondString;
+        private System.Windows.Forms.TextBox textBoxForTheSecond;
     }
 }
--- Лабораторная 1/VKLab1/VKLab1/Calculator.cs
+++ Лабораторная 1/VKLab1/VKLab1/Calculator.cs
@@ -16,6 +16,5 @@ namespace VKLab1
         {
             InitializeComponent();
         }
-
     }
 }
