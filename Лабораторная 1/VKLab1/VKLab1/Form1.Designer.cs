3213c9783b0809a09f3ddd90a6ef5a1bd231a751
Vasilisa Kashperko <90206134+VasilisaKashperko@users.noreply.github.com>
2022-02-07 22:54:48 +0300

    Interface for lab1 almost done

--- Лабораторная 1/VKLab1/VKLab1/Form1.Designer.cs
+++ Лабораторная 1/VKLab1/VKLab1/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace VKLab1
 {
-    partial class Form1
+    partial class Calculator
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,13 +28,294 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Calculator));
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.labelSM4 = new System.Windows.Forms.Label();
+            this.labelSM3 = new System.Windows.Forms.Label();
+            this.labelSM2 = new System.Windows.Forms.Label();
+            this.labelSM1 = new System.Windows.Forms.Label();
+            this.textBoxBust = new System.Windows.Forms.TextBox();
+            this.textBoxWaist = new System.Windows.Forms.TextBox();
+            this.textBoxHip = new System.Windows.Forms.TextBox();
+            this.textBoxHeight = new System.Windows.Forms.TextBox();
+            this.labelWaist = new System.Windows.Forms.Label();
+            this.labelHip = new System.Windows.Forms.Label();
+            this.labelBust = new System.Windows.Forms.Label();
+            this.labelHeight = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelClothes = new System.Windows.Forms.Label();
+            this.labelShoes = new System.Windows.Forms.Label();
+            this.labelFeet = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelSM5 = new System.Windows.Forms.Label();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.mainPanel.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(206)))), ((int)(((byte)(109)))), ((int)(((byte)(177)))));
+            this.mainPanel.Controls.Add(this.buttonCalculate);
+            this.mainPanel.Controls.Add(this.labelSM5);
+            this.mainPanel.Controls.Add(this.textBox1);
+            this.mainPanel.Controls.Add(this.labelFeet);
+            this.mainPanel.Controls.Add(this.labelShoes);
+            this.mainPanel.Controls.Add(this.labelClothes);
+            this.mainPanel.Controls.Add(this.labelSM4);
+            this.mainPanel.Controls.Add(this.labelSM3);
+            this.mainPanel.Controls.Add(this.labelSM2);
+            this.mainPanel.Controls.Add(this.labelSM1);
+            this.mainPanel.Controls.Add(this.textBoxBust);
+            this.mainPanel.Controls.Add(this.textBoxWaist);
+            this.mainPanel.Controls.Add(this.textBoxHip);
+            this.mainPanel.Controls.Add(this.textBoxHeight);
+            this.mainPanel.Controls.Add(this.labelWaist);
+            this.mainPanel.Controls.Add(this.labelHip);
+            this.mainPanel.Controls.Add(this.labelBust);
+            this.mainPanel.Controls.Add(this.labelHeight);
+            this.mainPanel.Controls.Add(this.panel3);
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.Location = new System.Drawing.Point(0, 0);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(839, 491);
+            this.mainPanel.TabIndex = 0;
+            // 
+            // labelSM4
+            // 
+            this.labelSM4.AutoSize = true;
+            this.labelSM4.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelSM4.Location = new System.Drawing.Point(375, 349);
+            this.labelSM4.Name = "labelSM4";
+            this.labelSM4.Size = new System.Drawing.Size(38, 27);
+            this.labelSM4.TabIndex = 16;
+            this.labelSM4.Text = "см";
+            // 
+            // labelSM3
+            // 
+            this.labelSM3.AutoSize = true;
+            this.labelSM3.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelSM3.Location = new System.Drawing.Point(375, 296);
+            this.labelSM3.Name = "labelSM3";
+            this.labelSM3.Size = new System.Drawing.Size(38, 27);
+            this.labelSM3.TabIndex = 15;
+            this.labelSM3.Text = "см";
+            // 
+            // labelSM2
+            // 
+            this.labelSM2.AutoSize = true;
+            this.labelSM2.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelSM2.Location = new System.Drawing.Point(375, 244);
+            this.labelSM2.Name = "labelSM2";
+            this.labelSM2.Size = new System.Drawing.Size(38, 27);
+            this.labelSM2.TabIndex = 14;
+            this.labelSM2.Text = "см";
+            // 
+            // labelSM1
+            // 
+            this.labelSM1.AutoSize = true;
+            this.labelSM1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelSM1.Location = new System.Drawing.Point(375, 193);
+            this.labelSM1.Name = "labelSM1";
+            this.labelSM1.Size = new System.Drawing.Size(38, 27);
+            this.labelSM1.TabIndex = 13;
+            this.labelSM1.Text = "см";
+            // 
+            // textBoxBust
+            // 
+            this.textBoxBust.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxBust.Location = new System.Drawing.Point(241, 343);
+            this.textBoxBust.Name = "textBoxBust";
+            this.textBoxBust.Size = new System.Drawing.Size(128, 34);
+            this.textBoxBust.TabIndex = 12;
+            // 
+            // textBoxWaist
+            // 
+            this.textBoxWaist.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxWaist.Location = new System.Drawing.Point(241, 290);
+            this.textBoxWaist.Name = "textBoxWaist";
+            this.textBoxWaist.Size = new System.Drawing.Size(128, 34);
+            this.textBoxWaist.TabIndex = 11;
+            // 
+            // textBoxHip
+            // 
+            this.textBoxHip.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxHip.Location = new System.Drawing.Point(241, 238);
+            this.textBoxHip.Name = "textBoxHip";
+            this.textBoxHip.Size = new System.Drawing.Size(128, 34);
+            this.textBoxHip.TabIndex = 10;
+            // 
+            // textBoxHeight
+            // 
+            this.textBoxHeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxHeight.Location = new System.Drawing.Point(241, 187);
+            this.textBoxHeight.Name = "textBoxHeight";
+            this.textBoxHeight.Size = new System.Drawing.Size(128, 34);
+            this.textBoxHeight.TabIndex = 9;
+            // 
+            // labelWaist
+            // 
+            this.labelWaist.AutoSize = true;
+            this.labelWaist.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelWaist.Location = new System.Drawing.Point(34, 292);
+            this.labelWaist.Name = "labelWaist";
+            this.labelWaist.Size = new System.Drawing.Size(190, 31);
+            this.labelWaist.TabIndex = 8;
+            this.labelWaist.Text = "Обхват талии:";
+            // 
+            // labelHip
+            // 
+            this.labelHip.AutoSize = true;
+            this.labelHip.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelHip.Location = new System.Drawing.Point(34, 240);
+            this.labelHip.Name = "labelHip";
+            this.labelHip.Size = new System.Drawing.Size(185, 31);
+            this.labelHip.TabIndex = 7;
+            this.labelHip.Text = "Обхват груди:";
+            // 
+            // labelBust
+            // 
+            this.labelBust.AutoSize = true;
+            this.labelBust.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelBust.Location = new System.Drawing.Point(34, 344);
+            this.labelBust.Name = "labelBust";
+            this.labelBust.Size = new System.Drawing.Size(184, 31);
+            this.labelBust.TabIndex = 6;
+            this.labelBust.Text = "Обхват бёдер:";
+            // 
+            // labelHeight
+            // 
+            this.labelHeight.AutoSize = true;
+            this.labelHeight.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelHeight.Location = new System.Drawing.Point(34, 189);
+            this.labelHeight.Name = "labelHeight";
+            this.labelHeight.Size = new System.Drawing.Size(137, 31);
+            this.labelHeight.TabIndex = 5;
+            this.labelHeight.Text = "Ваш рост:";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(143)))), ((int)(((byte)(33)))), ((int)(((byte)(110)))));
+            this.panel3.Controls.Add(this.labelName);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(839, 100);
+            this.panel3.TabIndex = 2;
+            // 
+            // labelName
+            // 
+            this.labelName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelName.ForeColor = System.Drawing.Color.White;
+            this.labelName.Location = new System.Drawing.Point(133, 29);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(573, 45);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Калькулятор женких размеров";
+            // 
+            // labelClothes
+            // 
+            this.labelClothes.AutoSize = true;
+            this.labelClothes.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelClothes.Location = new System.Drawing.Point(176, 123);
+            this.labelClothes.Name = "labelClothes";
+            this.labelClothes.Size = new System.Drawing.Size(117, 35);
+            this.labelClothes.TabIndex = 17;
+            this.labelClothes.Text = "Одежда";
+            // 
+            // labelShoes
+            // 
+            this.labelShoes.AutoSize = true;
+            this.labelShoes.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelShoes.Location = new System.Drawing.Point(586, 123);
+            this.labelShoes.Name = "labelShoes";
+            this.labelShoes.Size = new System.Drawing.Size(97, 35);
+            this.labelShoes.TabIndex = 18;
+            this.labelShoes.Text = "Обувь";
+            // 
+            // labelFeet
+            // 
+            this.labelFeet.AutoSize = true;
+            this.labelFeet.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelFeet.Location = new System.Drawing.Point(441, 189);
+            this.labelFeet.Name = "labelFeet";
+            this.labelFeet.Size = new System.Drawing.Size(195, 31);
+            this.labelFeet.TabIndex = 19;
+            this.labelFeet.Text = "Длина ступни:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(664, 188);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(106, 34);
+            this.textBox1.TabIndex = 20;
+            // 
+            // labelSM5
+            // 
+            this.labelSM5.AutoSize = true;
+            this.labelSM5.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelSM5.Location = new System.Drawing.Point(776, 195);
+            this.labelSM5.Name = "labelSM5";
+            this.labelSM5.Size = new System.Drawing.Size(38, 27);
+            this.labelSM5.TabIndex = 21;
+            this.labelSM5.Text = "см";
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonCalculate.Location = new System.Drawing.Point(302, 413);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(250, 47);
+            this.buttonCalculate.TabIndex = 22;
+            this.buttonCalculate.Text = "Рассчитать!";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(839, 491);
+            this.Controls.Add(this.mainPanel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Calculator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Калькулятор женских размеров";
+            this.mainPanel.ResumeLayout(false);
+            this.mainPanel.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label labelWaist;
+        private System.Windows.Forms.Label labelHip;
+        private System.Windows.Forms.Label labelBust;
+        private System.Windows.Forms.Label labelHeight;
+        private System.Windows.Forms.TextBox textBoxBust;
+        private System.Windows.Forms.TextBox textBoxWaist;
+        private System.Windows.Forms.TextBox textBoxHip;
+        private System.Windows.Forms.TextBox textBoxHeight;
+        private System.Windows.Forms.Label labelSM4;
+        private System.Windows.Forms.Label labelSM3;
+        private System.Windows.Forms.Label labelSM2;
+        private System.Windows.Forms.Label labelSM1;
+        private System.Windows.Forms.Label labelClothes;
+        private System.Windows.Forms.Label labelShoes;
+        private System.Windows.Forms.Label labelSM5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label labelFeet;
+        private System.Windows.Forms.Button buttonCalculate;
     }
 }
 
--- Лабораторная 1/VKLab1/VKLab1/Form1.cs
+++ Лабораторная 1/VKLab1/VKLab1/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace VKLab1
 {
-    public partial class Form1 : Form
+    public partial class Calculator : Form
     {
-        public Form1()
+        public Calculator()
         {
             InitializeComponent();
         }
